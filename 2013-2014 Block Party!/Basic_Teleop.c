5b71b04811cfe093c58aa5f1d358c6ca07ce69f0
Max <ftc5602>
2013-10-13 14:12:06 -0400

    switched motor left/right

--- 2013-2014 Block Party!/Basic_Teleop.c
+++ 2013-2014 Block Party!/Basic_Teleop.c
@@ -8,15 +8,15 @@
 //comment
 // even more comments
 #include <JoystickDriver.c>
-int threshold = 8;
+int threshold = 12;
 
 task main()
 {
 	while (1)
 	{
 		getJoystickSettings(joystick);
-	motor [leftDrive] = abs(joystick.joy1_y1) > threshold ? joystick.joy1_y1 : 0;
-	motor [rightDrive] = abs(joystick.joy1_y2) > threshold ? joystick.joy1_y2 : 0;
+	motor [leftDrive] = abs(joystick.joy1_y2) > threshold ? joystick.joy1_y1 : 0;
+	motor [rightDrive] = abs(joystick.joy1_y1) > threshold ? joystick.joy1_y2 : 0;
 #if 0
 	if (joy1Btn(7) == 1) {
 			motor [harvesterMover] = (joy1Btn(5) == 1 || joy1Btn(6) == 1) ? 50 : 25;
