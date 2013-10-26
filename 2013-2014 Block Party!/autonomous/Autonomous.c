8112ee051224d9c6f31301f523edd61cc1df322e
Max <ftc5602>
2013-10-26 13:44:22 -0400

    removed non-current code

--- 2013-2014 Block Party!/autonomous/Autonomous.c
+++ 2013-2014 Block Party!/autonomous/Autonomous.c
@@ -7,6 +7,7 @@
 
 #include <JoystickDriver.c>
 #include <drivers/hitechnic-irseeker-v2.h>
+
 void initializeRobot ()
 {
 }
@@ -21,7 +22,8 @@ void allStop ()
 void timedDrive (int rightSpeed, int leftSpeed, int driveTime)
 {
 	ClearTimer(T2);
-	while (time1 [T2] < driveTime) {
+	while (time1 [T2] < driveTime)
+	{
 		motor [rightDrive] = rightSpeed;
 		motor [leftDrive] = leftSpeed;
 	}
@@ -37,10 +39,8 @@ void drive (int rightSpeed, int leftSpeed)
 void timedRightPointTurn (int turnSpeed, int turnTime)
 {
 	ClearTimer(T3);
-	while (time1 [T3] < turnTime) {
-		//motor [leftDrive] = -1 * turnSpeed;
-		//motor [rightDrive] = turnSpeed;
-	//simplified code below
+	while (time1 [T3] < turnTime)
+	{
 		drive(-1 * turnSpeed, turnSpeed);
 	}
 	allStop();
@@ -49,10 +49,8 @@ void timedRightPointTurn (int turnSpeed, int turnTime)
 void timedLeftPointTurn (int turnSpeed, int turnTime)
 {
 	ClearTimer(T4);
-	while (time1 [T4] < turnTime) {
-		//motor [leftDrive] = turnSpeed;
-		//motor [rightDrive] = -1 * turnSpeed;
-		//simplified code below
+	while (time1 [T4] < turnTime)
+	{
 		drive(turnSpeed, -1 * turnSpeed);
 	}
 	allStop();
@@ -60,36 +58,29 @@ void timedLeftPointTurn (int turnSpeed, int turnTime)
 
 void rightPointTurn (int turnSpeed)
 {
-	//motor [leftDrive] = -1 * turnSpeed;
-	//motor [rightDrive] = turnSpeed;
-	//simplified code below
 	drive(turnSpeed, -1 * turnSpeed);
 }
 
 void leftPointTurn (int turnSpeed)
 {
-	//motor [leftDrive] = turnSpeed;
-	//motor [rightDrive] = -1 * turnSpeed;
-//simplified code below
-		drive(-1 * turnSpeed, turnSpeed);
+	drive(-1 * turnSpeed, turnSpeed);
 }
 
 void alignWithBeacon ()
 {
-		while (SensorValue [irSensor] !=5)
+	while (SensorValue [irSensor] !=5)
 	{
 		nxtDisplayCenteredBigTextLine(4 ,"%d", SensorValue [irSensor]);
 		if (SensorValue [irSensor] > 5) {
-			rightPointTurn(50);
+			rightPointTurn(25);
 		}
-		/*added else*/ else if (SensorValue [irSensor] < 5) {
-			leftPointTurn(50);
-		} else {
-		/*removed if replaced with else*/ /*if (SensorValue [irSensor] == 5) {*/
+		else if (SensorValue [irSensor] < 5) {
+			leftPointTurn(25);
+			} else {
 			allStop();
+			return;
 		}
 	}
-	allStop();
 }
 
 void driveToBeacon (int distanceFromBeacon)
@@ -103,58 +94,8 @@ void driveToBeacon (int distanceFromBeacon)
 
 task main()
 {
-
 	initializeRobot();
 
 	alignWithBeacon();
 	driveToBeacon(45);
-
-	wait1Msec(2500);
-
-	timedDrive(-75, -75, 1000);
-	timedRightPointTurn(100, 450);
-	//timedLeftPointTurn(100, 450);
-	timedDrive(100, 100, 1500);
-	allStop();
-
-#if 0
-	while (SensorValue [irSensor] !=5)
-	{
-		nxtDisplayCenteredBigTextLine(4 ,"%d", SensorValue [irSensor]);
-		if (SensorValue [irSensor] > 5) {
-			rightPointTurn(50);
-		}
-		if (SensorValue [irSensor] < 5) {
-			leftPointTurn(50);
-		}
-		if (SensorValue [irSensor] == 5) {
-			allStop();
-		}
-	}
-	allStop();
-#elif 0
-	nxtDisplayCenteredBigTextLine(4 ,"%d", SensorValue [irSensor]);
-	if (SensorValue [irSensor] > 5) {
-		motor [leftDrive] = -50;
-		motor [rightDrive] = 50;
-	}
-	if (SensorValue [irSensor] < 5) {
-		motor [leftDrive] = 50;
-		motor [rightDrive] = -50;
-	}
-	if (SensorValue [irSensor] == 5) {
-		motor [rightDrive] = 0;
-		motor [leftDrive] = 0;
-	}
-#endif
-
-//basic driving
-
-#if 0
-	waitForStart();
-	timedDrive(100, 100, 2500);
-	timedRightPointTurn (50, 1100);
-	timedDrive(100, 100, 3000);
-#endif
-
 }
