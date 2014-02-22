aa09eeac84d6a113aa326293b9475f1e27befd61
Max <ftc5602>
2014-02-22 18:13:10 -0500

    add special end game drive mode and clean up excess code in teleop program also add new tank drive program for demo bot

--- 2013-2014 Block Party!/teleop/Tank.c
+++ 2013-2014 Block Party!/teleop/Tank.c
@@ -9,10 +9,11 @@
 task main()
 {
 	const int t = 8;
+	const float standardDriveScale = 0.79
 	while (1)
 	{
 		getJoystickSettings(joystick);
-		motor [leftDrive] = (abs(joystick.joy1_y1) > t) ? joystick.joy1_y1 : 0;
-		motor [rightDrive] = (abs(joystick.joy1_y2) > t) ? joystick.joy1_y2 : 0;
+		motor [leftDrive] = (abs(joystick.joy1_y1) > t) ? joystick.joy1_y1 * standardDriveScale : 0;
+		motor [rightDrive] = (abs(joystick.joy1_y2) > t) ? joystick.joy1_y2 * standardDriveScale: 0;
 	}
 }
